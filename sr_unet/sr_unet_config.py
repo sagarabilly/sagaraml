b00f76d489976f032d8b7797bb52edabb307b259
Billy Sagara <135346372+sagarabilly@users.noreply.github.com>
2025-04-15 15:07:05 +0900

    Update sr_unet_config.py

--- sr_unet/sr_unet_config.py
+++ sr_unet/sr_unet_config.py
@@ -10,5 +10,5 @@ checkpoint_path = r"D:\image_segmentation\saved_model"
 
 custom_data_path = r"D:\image_segmentation\data.csv"
 
-custom_data_folder = r"D:\Code_Script\moyamoya\train"
-custom_valid_folder = r"D:\Code_Script\moyamoya\val"
+custom_data_folder = r"D:\image_segmentation\dataset\train"
+custom_valid_folder = r"D:\image_segmentation\dataset\val"
